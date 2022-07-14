861c01f48024c87354aea5eac535b320e643e51c
ts1982 <t.saito55555@gmail.com>
2022-07-14 18:33:09 +0900

    01 first commit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,13 @@
+<div class="container ml-3">
+    <h3>AAA</h3>
+    <ul>
+        <li><a href="#">aaa</a></li>
+        <li><a href="#">bbb</a></li>
+        <li><a href="#">ccc</a></li>
+    </ul>
+    <h3>BBB</h3>
+    <ul>
+        <li><a href="#">ddd</a></li>
+        <li><a href="#">eee</a></li>
+    </ul>
+</div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>SalesMaker</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/d5001603e3.js" crossorigin="anonymous"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        @component('components.header')
+        @endcomponent
+
+        <main class="py-4 row">
+            <div class="sidebar col-md-2 shadow-sm">
+                @component('components.sidebar')
+                @endcomponent
+            </div>
+            <div class="col-md-8">
+                @yield('content')
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
